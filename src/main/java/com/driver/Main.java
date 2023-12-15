d6d9d7ab66f289d0adc7eb186c72afc2cc4841b5
mangeshtekade00 <mangeshtekade00@gmail.com>
2023-12-15 12:20:41 +0530

    add to repo

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+
+    }
+    public static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
+
+    public static void main(String [] args){
+        B ab = new B();
+        System.out.println(ab.meth());
+        B abc = new B();
+        System.out.println(abc.meth());
+
+    }
+
 }
